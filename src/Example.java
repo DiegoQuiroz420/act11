6d977c322976e1ad806849350e1a74ff4471fe16
DiegoQuiroz420 <al02994936@tecmilenio.mx>
2023-04-14 03:04:40 -0600

    Initial commit

---
+++ src/Example.java
@@ -0,0 +1,34 @@
+public class Example {
+
+    public static void main(String[] args) {
+        // Error
+        Shape shape = new Shape() {
+            @Override
+            public double area() {
+                return 0;
+            }
+
+            @Override
+            public double perimeter() {
+                return 0;
+            }
+        };
+        System.out.println(shape.area());
+        System.out.println(shape.perimeter());
+        // Comentar para continuar con la ejecucion
+
+        Circle circle = new Circle(5);
+        System.out.println(circle.area());
+        System.out.println(circle.perimeter());
+
+        Square square = new Square(10);
+        System.out.println(square.area());
+        System.out.println(square.perimeter());
+
+        EquilateralTriangle triangle = new EquilateralTriangle(10);
+        System.out.println(triangle.area());
+        System.out.println(triangle.perimeter());
+
+    }
+
+}
